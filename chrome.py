39086bb9bc7c654283fe26e2f325e941e97a85cc
GuiMCL <113605222+GuiMCL@users.noreply.github.com>
2023-09-28 11:07:50 -0300

    UPDATE

--- chrome.py
+++ chrome.py
@@ -13,7 +13,7 @@ import threading
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Guilherme Maciel\Desktop\bot cot\sla\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\User\Desktop\bot cantelle")
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
@@ -59,20 +59,20 @@ class Interface:
             font=("Inter SemiBold", 12 * -1)
         )
 
-        entry_image_1 = PhotoImage(
+        self.entry_image_1 = PhotoImage(
             file=relative_to_assets("entry_1.png"))
-        entry_bg_1 = canvas.create_image(
+        self.entry_bg_1 = canvas.create_image(
             303.0,
             102.5,
-            image=entry_image_1
+            image=self.entry_image_1
         )
-        entry_1 = Entry(
+        self.entry_1 = Entry(
             bd=0,
             bg="#FFFFFF",
             fg="#000716",
             highlightthickness=0
         )
-        entry_1.place(
+        self.entry_1.place(
             x=126.0,
             y=87.0,
             width=354.0,
@@ -88,36 +88,36 @@ class Interface:
             font=("Inter SemiBold", 12 * -1)
         )
 
-        entry_image_2 = PhotoImage(
+        self.entry_image_2 = PhotoImage(
             file=relative_to_assets("entry_2.png"))
         entry_bg_2 = canvas.create_image(
             300.0,
             205.0,
-            image=entry_image_2
+            image=self.entry_image_2
         )
-        entry_2 = Entry(
+        self.entry_2 = Entry(
             bd=0,
             bg="#FFFFFF",
             fg="#000716",
             highlightthickness=0
         )
-        entry_2.place(
+        self.entry_2.place(
             x=126.0,
             y=183.0,
             width=348.0,
             height=42.0
         )
 
-        button_image_1 = PhotoImage(
+        self.button_image_1 = PhotoImage(
             file=relative_to_assets("button_1.png"))
-        button_1 = Button(
-            image=button_image_1,
+        self.button_1 = Button(
+            image=self.button_image_1,
             borderwidth=0,
             highlightthickness=0,
             command= lambda:threading.Thread(target=self.get).start(),
             relief="flat"
         )
-        button_1.place(
+        self.button_1.place(
             x=111.0,
             y=272.0,
             width=386.0,
@@ -129,17 +129,19 @@ class Interface:
 
 
     def get(self):
-        Navegador.get("https://csc.princesadoscampos.wtmh.com.br/login")
-        email2 = "{}".format(self.input_from.get())
-        senha2= "{}".format(self.input_from_uf.get())
+        Navegador.get("https://csc.princesadoscampos.wtmh.com.br/aluno/workflow/entrada/t474")
+        email2 = "{}".format(self.entry_1.get())
+        senha2= "{}".format(self.entry_2.get())
         email = email2
         senha = senha2
 
         campoemail = Navegador.find_element(By.CSS_SELECTOR , '#login_username')
-        sleep(1)
         camposenha = Navegador.find_element(By.CSS_SELECTOR , '#login_password')
-        sleep(1)
         botaoentrar = Navegador.find_element(By.XPATH , '//*[@id="form"]/div[3]/input')
+        sleep(5)
+        
+        
+        
 
 
 
@@ -148,6 +150,10 @@ class Interface:
         camposenha.send_keys(senha2)
         botaoentrar.click()
         sleep(10)
+        botaomenu = Navegador.find_element(By.XPATH, '//*[@id="empresa_relacionada_row"]/div/div[2]/div[2]/div/div[3]')
+        
+        botaomenu.click()
+
    
 
 
